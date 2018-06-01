9595de0bfdc7159307093e51cc380802727f7a97
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2018-06-01 16:13:51 -0600

    production setup

--- lib/hello_web/channels/user_socket.ex
+++ lib/hello_web/channels/user_socket.ex
@@ -6,6 +6,7 @@ defmodule HelloWeb.UserSocket do
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
+  timeout: 28_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
